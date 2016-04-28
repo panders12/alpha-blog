fcf040fb71c289ee89b6dfd55e0e3a553054a755
panders <pascal.anders@outlook.com>
2016-04-28 13:51:33 +0000

    add edit functionality for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated succesfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
 
   private
   
